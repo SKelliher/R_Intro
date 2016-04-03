1d4a9a0efb1ffc978199783deb00a2fd5db9bacc
St John Kelliher <stjmk63@gmail.com>
2016-04-03 18:55:21 -0400

    Added files via upload

---
+++ R_True_Basics.R
@@ -0,0 +1,79 @@
+# Start Script
+print("Starting Script")
+
+# Assign variables height and width
+height <- 3
+width <- 6 
+
+# Calculate area
+area <- height * width
+
+# Print the area
+area
+
+# Remove area from workspace
+rm(area)
+
+# List workspace
+ls()
+
+
+# Sample Calculations from Exercises
+
+# Addition
+5 + 5 
+
+# Subtraction
+5 - 5 
+
+# Multiplication
+3 * 5
+
+# Division
+(5 + 5) / 2 
+
+# Exponentiation
+2^5
+
+# Modulo
+28 %% 6
+
+# Clear the entire workspace
+rm(list = ls())
+
+# List the contents of your workspace
+ls()
+
+# Create the variable horses
+horses <- 3
+
+# Create the variable dogs
+dogs <- 7
+
+# Create the variable animals
+animals <- horses + dogs
+
+# Inspect the contents of the workspace again
+ls()
+
+# Remove dogs from the workspace
+rm(dogs)
+
+# Inspect the objects in your workspace once more
+ls()
+
+# Create the variables r and R
+r <- 2
+R <- 6
+
+
+# Calculate the volume of the donut: vol_donut
+vol_donut <- 2 * pi^2 * r^2 * R
+
+# Remove all intermediary variables that you've used with rm()
+rm(r)
+rm(R)
+
+
+# List the elements in your workspace
+ls()
